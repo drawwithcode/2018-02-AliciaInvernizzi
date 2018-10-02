77bddd0902ff1acdf145ab6b62d36a01b5983c8b
Alicia <Alicia@MacBook-Pro-di-Alicia.local>
2018-10-02 10:09:10 +0200

    Final project

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
   createCanvas(windowWidth,windowHeight);
   background(126,71,191);
   angleMode(DEGREES);
-  frameRate(60);
+  frameRate(70);
 }
 
 function draw() {
@@ -15,25 +15,136 @@ function draw() {
   stroke(255,128,112);
   strokeWeight(1);
 
-  push();
-  translate(width/2,height/2);
-  rotate(frameCount);
-
-  line(sin(frameCount)*280,cos(frameCount)*280,cos(90)*280,sin(90)*280);
-  pop();
+  // push();
+  // translate(width/2,height/2);
+  // rotate(frameCount);
+  //
+  // line(sin(frameCount)*100,cos(frameCount)*100,cos(90)*100,sin(90)*100);
+  // pop();
 
   // push();
-  // stroke(lerpColor(color('#ffd96b'), color('#ffa5c4'),frameCount/360));
-  // translate(width/2,height/2-50);
+  // stroke(255,165,196,90);
+  //
+  // translate(width/2,height/2);
   // rotate(frameCount);
-  // translate(-200,0);
-  // line(sin(frameCount)*150,cos(frameCount)*150,cos(90)*150,sin(90)*150);
+  //
+  // line(180,0,cos(0),sin(0));
   // pop();
+
+  push();
+  stroke(255,255,255,20);
+
+  translate(width/2,height/2);
+  rotate(frameCount*2);
+
+  line(570,0,cos(0),sin(0));
+  pop();
+
+
+
+  line(180,0,cos(0),sin(0));
+  pop();
+
+  push();
+  stroke(255,255,255,30);
+
+  translate(width/2,height/2);
+  rotate(frameCount*2);
+
+  line(470,0,cos(0),sin(0));
+  pop();
+
+
+
+  line(180,0,cos(0),sin(0));
+  pop();
+
+  push();
+  stroke(255,255,255,40);
+
+  translate(width/2,height/2);
+  rotate(frameCount*2);
+
+  line(370,0,cos(0),sin(0));
+  pop();
+
+
+
+
+
+
+
+
+
+
+  push();
+  stroke(lerpColor(color('#FF8070'), color('#FFD96B'),frameCount/360));
+
+  translate(width/2-100,height/2-100);
+  rotate(45);
+  rotate(frameCount);
+  translate(100, 0);
+  rotate(frameCount);
+  translate(-100,0);
+
+  line(100,0,cos(90),sin(90));
+  pop();
+
+
+
+
+  push();
+  stroke(lerpColor(color('#FF8070'), color('#FFD96B'),frameCount/360));
+
+  translate(width/2+100,height/2-100);
+  rotate(135);
+  rotate(frameCount);
+  translate(100, 0);
+  rotate(frameCount);
+  translate(-100,0);
+
+  line(100,0,cos(90),sin(90));
+  pop();
+
+
+  push();
+  stroke(lerpColor(color('#FF8070'), color('#FFD96B'),frameCount/360));
+
+  translate(width/2+100,height/2+100);
+  rotate(-135);
+  rotate(frameCount);
+  translate(100, 0);
+  rotate(frameCount);
+  translate(-100,0);
+
+  line(100,0,cos(90),sin(90));
+  pop();
+
+
+
+
+  push();
+  stroke(lerpColor(color('#FF8070'), color('#FFD96B'),frameCount/360));
+
+  translate(width/2-100,height/2+100);
+  rotate(-45);
+  rotate(frameCount);
+  translate(100, 0);
+  rotate(frameCount);
+  translate(-100,0);
+
+  line(100,0,cos(90),sin(90));
+  pop();
+
+
 
 
   if (frameCount == 360) {
     noLoop();
   }
+
+
+
 
 
 }
